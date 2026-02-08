3aa93a6d66ccd4fef3d7014e177525fc73a06f42
maneedasshrestha <shresthamaneedas852@gmail.com>
2026-02-08 14:21:12 +0545

    feat(): login and signup

--- app/custom_components/Navbar.tsx
+++ app/custom_components/Navbar.tsx
@@ -90,14 +90,7 @@ const Navbar = () => {
 
           <div className="hidden lg:ml-auto lg:flex lg:items-center lg:space-x-10">
             <a
-              href="#"
-              title=""
-              className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-            >
-              Organizer Login
-            </a>
-            <a
-              href="/sign-up"
+              href="/login"
               title=""
               className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
               role="button"
@@ -135,16 +128,9 @@ const Navbar = () => {
                   {" "}
                   Automation{" "}
                 </a>
+
                 <a
-                  href="#"
-                  title=""
-                  className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-                >
-                  {" "}
-                  Organizer Login{" "}
-                </a>
-                <a
-                  href="#"
+                  href="/login"
                   title=""
                   className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   role="button"
--- app/sign-up/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LoginPage from "../login/page";
-
-const SignUpPage = () => {
-  return <LoginPage />;
-};
-
-export default SignUpPage;
